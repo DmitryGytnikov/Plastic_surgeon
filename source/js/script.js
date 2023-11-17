e0fe483bd9e6871ea2a0c6dfbcd4e2b57ac75790
DmitryGytnikov <gytnikovd@mail.ru>
2023-11-17 16:01:06 +0300

    Add slider"Акции" .action-slider на service.html

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,43 @@
 'use strict';
+
+// Слайдер "Акции" .action-slider на service.html
+
+const actionSliders = document.querySelectorAll(".swiper-container.action-slider");
+
+actionSliders.forEach((el) => {
+  let myActionSliders = new Swiper(el, {
+    slideToClickedSlide: true,
+    initialSlide : 1,
+    pagination: {
+      el: el.querySelector(".swiper-pagination"),
+      type: 'bullets',
+      clickable: true,
+    },
+    dynamicBullets: true,
+    breakpoints: {
+    // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        navigation: {
+          nextEl: el.querySelector(".swiper-button-next"),
+          prevEl: el.querySelector(".swiper-button-prev"),
+        },
+      },
+      // when window width is >= 480px
+      780: {
+        // centeredSlides: false,
+        centeredSlides: true,
+        slidesPerView: 2,
+        // spaceBetween: 0,
+        spaceBetween: 0,
+      },
+      // when window width is >= 1680px
+      1680: {
+        centeredSlides: true,
+        slidesPerView: 3,
+        spaceBetween: 78.5,
+      }
+    }
+  })
+});
